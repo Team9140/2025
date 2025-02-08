b66caf4f2878009eec74fe5d1eb782fa0ebaf41f
Gijs Landwehr <gijslandwehr@gmail.com>
2025-02-08 16:17:35 -0600

    add notes for elevator and intake

---
+++ src/main/java/org/team9140/frc2025/subsystems/Elevator.java
@@ -0,0 +1,20 @@
+package org.team9140.frc2025.subsystems;
+
+// make a subsystem
+public class Elevator {
+    
+// two talonFX's (one main, one follower)
+// set up MotionMagicVoltage control request (see 2024 Arm.java for example)
+// make up gear ratio for a conversion factor 
+// config current limit
+
+
+// canDI
+// use as remote limit switch for main TalonFX motor
+
+// needs command to go to position
+// take a Distance, not a double
+
+
+
+}
