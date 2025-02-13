d0cb5bb4440bd00087d7ed84ec10b8f0bc7b5330
srinidhisarthy <srinidhisarthy@gmail.com>
2025-02-12 18:40:46 -0600

    elevator (incomplete)

--- src/main/java/org/team9140/frc2025/Constants.java
+++ src/main/java/org/team9140/frc2025/Constants.java
@@ -1,6 +1,8 @@
 package org.team9140.frc2025;
 
+import com.ctre.phoenix6.signals.InvertedValue;
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.LinearVelocity;
 
 import static edu.wpi.first.units.Units.*;
@@ -9,5 +11,23 @@ public class Constants {
     public static class Drive {
         public static final LinearVelocity MIN_TRANSLATE_MPS = MetersPerSecond.of(0.01);
         public static final AngularVelocity MIN_ROTATE_RPS = DegreesPerSecond.of(1);
+    }
+
+    public static class ArmPositions{
+        // Positions in radians
+        public static final double INTAKE = -1.7;
+        // Motion Magic Specific Limit
+        public static final double FEED_FORWARD = 0.0;
+
+        public static final double MAX_CURRENT = 48.0;
+        // Radian rotations of arm
+        public static final double SENSOR_TO_MECHANISM_RATIO = 80.0 / 9.0 * 58.0 / 11.0 / (2 * Math.PI);
+    }
+
+    public static class Elevator {
+        public static final double GEAR_RATIO = 1.0;
+        public static final Distance SPOOL_RADIUS = Inches.of(1);
+        public static final double METERS_PER_MOTOR_ROTATION = SPOOL_RADIUS.in(Meters) * Math.PI * 2.0 / GEAR_RATIO;
+        public static final InvertedValue LEFT_MOTOR_INVERT = InvertedValue.Clockwise_Positive;
     }
 }
--- src/main/java/org/team9140/frc2025/subsystems/Elevator.java
+++ src/main/java/org/team9140/frc2025/subsystems/Elevator.java
@@ -1,20 +1,62 @@
 package org.team9140.frc2025.subsystems;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.HardwareLimitSwitchConfigs;
+import com.ctre.phoenix6.controls.CoastOut;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.hardware.CANdi;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.ForwardLimitSourceValue;
+import com.ctre.phoenix6.signals.ReverseLimitSourceValue;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import org.team9140.frc2025.Constants;
+
+import static edu.wpi.first.units.Units.Meters;
+
 // make a subsystem
-public class Elevator {
-    
-// two talonFX's (one main, one follower)
-// set up MotionMagicVoltage control request (see 2024 Arm.java for example)
-// make up gear ratio for a conversion factor 
+public class Elevator implements Subsystem {
+// two talonFXs (one main, one follower)     constants?
+    TalonFX talonMain = new TalonFX(52);
+    TalonFX talonFollower = new TalonFX(52);
+    CANdi elevatorCandi = new CANdi(0);
+    private final MotionMagicVoltage voltage;
+
+    public Elevator() {
+        // canDI
+        var limits = new HardwareLimitSwitchConfigs();
+        limits.ForwardLimitSource = ForwardLimitSourceValue.RemoteCANdiS1;
+        limits.ReverseLimitSource = ReverseLimitSourceValue.RemoteCANdiS2;
+
+        var feedback = new FeedbackConfigs();
+        feedback.withSensorToMechanismRatio(Constants.Elevator.METERS_PER_MOTOR_ROTATION);
+
+        // use as remote limit switch for main TalonFX motor
+        talonMain.getConfigurator().apply(limits);
+
+        // set up MotionMagicVoltage control request (see 2024 Arm.java for example)
+        this.voltage = new MotionMagicVoltage(Constants.ArmPositions.INTAKE)
+                .withEnableFOC(true)
+                .withSlot(0)
+                .withFeedForward(Constants.ArmPositions.FEED_FORWARD);
+    }
+
+// make up gear ratio for a conversion factor - units to radians
+    FeedbackConfigs feedbackConfigs = new FeedbackConfigs().withSensorToMechanismRatio(Constants.ArmPositions.SENSOR_TO_MECHANISM_RATIO);
+
 // config current limit
-
-
-// canDI
-// use as remote limit switch for main TalonFX motor
+    CurrentLimitsConfigs currentLimitConfig = new CurrentLimitsConfigs().withStatorCurrentLimit(Constants.ArmPositions.MAX_CURRENT).withStatorCurrentLimitEnable(true);
 
 // needs command to go to position
 // take a Distance, not a double
+    public Command toPostion(Distance distance) {
+        distance.in(Meters);
+        return this.runOnce(() -> this.voltage.withPosition(distance));
+    }
 
-
-
+    public Command disable() {
+        return this.runOnce(() -> this.talonMain.setControl(new CoastOut()));
+    }
 }
