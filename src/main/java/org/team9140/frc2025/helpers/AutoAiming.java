2e9262671085b8553710082445c223832f72d1c3
Urjith Mishra <66270347+TheScientist101@users.noreply.github.com>
2025-02-15 16:19:25 -0600

    Move all branches to one enum
    Signed-off-by: Urjith Mishra <66270347+TheScientist101@users.noreply.github.com>

--- src/main/java/org/team9140/frc2025/helpers/AutoAiming.java
+++ src/main/java/org/team9140/frc2025/helpers/AutoAiming.java
@@ -1,52 +1,49 @@
 package org.team9140.frc2025.helpers;
 
-import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Translation2d;
 import org.team9140.frc2025.Constants;
 
-import java.util.Arrays;
-
 public class AutoAiming {
-    static abstract class Reef {
-        public enum Branches {
-            A,
-            B,
-            C,
-            D,
-            E,
-            F,
-            G,
-            H,
-            I,
-            J,
-            K,
-            L
-        }
+    public enum Branches {
+        BlueH,
+        BlueI,
+        BlueJ,
+        BlueK,
+        BlueL,
+        BlueA,
+        BlueB,
+        BlueC,
+        BlueD,
+        BlueE,
+        BlueF,
+        BlueG,
+        RedA,
+        RedB,
+        RedC,
+        RedD,
+        RedE,
+        RedF,
+        RedG,
+        RedH,
+        RedI,
+        RedJ,
+        RedK,
+        RedL
     }
 
-    static class Red extends Reef {
-        public static Branches nearestBranch(Translation2d robotPose) {
-            double angle = robotPose.minus(Constants.FieldItemPoses.REEF_BLUE).getAngle().getRadians();
+    public static Branches getBranch(Translation2d pose) {
+        // Past Middle
+        if (pose.getX() < 17.548225 / 2) {
+            double angle = MathUtil.angleModulus(pose.minus(Constants.FieldItemPoses.REEF_BLUE).getAngle().unaryMinus().getRadians());
             Branches[] branches = Branches.values();
 
-            return branches[(int) (Math.TAU / branches.length * angle / (Math.TAU))];
-        }
-    }
-
-    static class Blue extends Reef {
-        public static Branches nearestBranch(Translation2d robotPose) {
-            double angle = -robotPose.minus(Constants.FieldItemPoses.REEF_BLUE).getAngle().getRadians();
-            Branches[] branches = Branches.values();
-
-            return branches[(int) (Math.TAU / branches.length * angle / (Math.TAU))];
-        }
-    }
-
-    public static Reef.Branches getBranch(Translation2d pose) {
-        if (pose.getX() > 17.548225) {
-            return Blue.nearestBranch(pose);
+            return branches[branches.length / 4 + (int) (branches.length / 2 * angle / (Math.TAU)) - 1];
         } else {
-            return Red.nearestBranch(pose);
+            double angle = pose.minus(Constants.FieldItemPoses.REEF_BLUE).getAngle().getRadians();
+            Branches[] branches = Branches.values();
+
+            return branches[24 - (int) (Math.TAU / branches.length * angle / (Math.TAU))];
         }
     }
 }
