f8936843320de33b07fb8d8bce5e0675818e5d5c
Urjith Mishra <66270347+TheScientist101@users.noreply.github.com>
2025-03-07 10:13:34 -0600

    Rename MazeRunner to FollowPath
    Signed-off-by: Urjith Mishra <66270347+TheScientist101@users.noreply.github.com>

--- src/main/java/org/team9140/frc2025/commands/AutonomousRoutines.java
+++ src/main/java/org/team9140/frc2025/commands/AutonomousRoutines.java
@@ -7,7 +7,7 @@ import org.team9140.frc2025.Constants;
 import org.team9140.frc2025.subsystems.CommandSwerveDrivetrain;
 import org.team9140.frc2025.subsystems.Elevator;
 import org.team9140.frc2025.subsystems.Manipulator;
-import org.team9140.lib.MazeRunner;
+import org.team9140.lib.FollowPath;
 
 public class AutonomousRoutines {
     private static final Command SCORE_CORAL_L4 = Elevator.getInstance().moveToPosition(Constants.Elevator.L4_coral_height).andThen(new WaitCommand(5.0).deadlineFor(Manipulator.getInstance().outtakeCoral()));
@@ -15,7 +15,7 @@ public class AutonomousRoutines {
     private static final Command INTAKE_CORAL = Manipulator.getInstance().intakeCoral();
 
     public static Command oneCoral(CommandSwerveDrivetrain drivetrain) {
-        MazeRunner path = new MazeRunner("oneCoral", drivetrain, DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue));
+        FollowPath path = new FollowPath("oneCoral", drivetrain, DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue));
         return path.gimmeCommand().andThen(new WaitCommand(5.0).deadlineFor(SCORE_CORAL_L4)).andThen(RESET_ARM);
     }
 
@@ -24,7 +24,7 @@ public class AutonomousRoutines {
     }
 
     public static Command feed(CommandSwerveDrivetrain drivetrain) {
-        MazeRunner path = new MazeRunner("feed", drivetrain, DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue));
+        FollowPath path = new FollowPath("feed", drivetrain, DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue));
         return path.gimmeCommand().andThen(INTAKE_CORAL);
     }
 }
