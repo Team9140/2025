ed4242ca1cb2d60c4f65f76864719a638090727b
Gijs Landwehr <gijslandwehr@gmail.com>
2025-03-07 12:58:48 -0600

    grab updates to auto routines

--- src/main/java/org/team9140/frc2025/commands/AutonomousRoutines.java
+++ src/main/java/org/team9140/frc2025/commands/AutonomousRoutines.java
@@ -10,11 +10,21 @@ import org.team9140.frc2025.subsystems.Manipulator;
 import org.team9140.lib.FollowPath;
 
 public class AutonomousRoutines {
-    private static final Command SCORE_CORAL_L1 = Elevator.getInstance().moveToPosition(Constants.Elevator.L1_coral_height).andThen(Manipulator.getInstance().outtakeCoral());
+    private static final Command SCORE_CORAL_L4 = Elevator.getInstance().moveToPosition(Constants.Elevator.L4_coral_height).andThen(new WaitCommand(5.0).deadlineFor(Manipulator.getInstance().outtakeCoral()));
     private static final Command RESET_ARM = Manipulator.getInstance().turnOff().andThen(Elevator.getInstance().moveToPosition(Constants.Elevator.STOW_height));
+    private static final Command INTAKE_CORAL = Manipulator.getInstance().intakeCoral();
 
     public static Command oneCoral(CommandSwerveDrivetrain drivetrain) {
         FollowPath path = new FollowPath("oneCoral", drivetrain, DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue));
-        return path.gimmeCommand().andThen(new WaitCommand(5.0).deadlineFor(SCORE_CORAL_L1)).andThen(RESET_ARM);
+        return path.gimmeCommand().andThen(new WaitCommand(5.0).deadlineFor(SCORE_CORAL_L4)).andThen(RESET_ARM);
+    }
+
+    public static Command feedOneCoral(CommandSwerveDrivetrain drivetrain) {
+        return oneCoral(drivetrain).andThen(feed(drivetrain));
+    }
+
+    public static Command feed(CommandSwerveDrivetrain drivetrain) {
+        FollowPath path = new FollowPath("feed", drivetrain, DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue));
+        return path.gimmeCommand().andThen(INTAKE_CORAL);
     }
 }
