227d81073e2f54eb10d0a45e32b66a1439309dd6
Urjith Mishra <66270347+TheScientist101@users.noreply.github.com>
2025-03-18 21:59:52 -0500

    Swap right motor for leader in elevator
    Signed-off-by: Urjith Mishra <66270347+TheScientist101@users.noreply.github.com>

--- src/main/java/org/team9140/frc2025/subsystems/Elevator.java
+++ src/main/java/org/team9140/frc2025/subsystems/Elevator.java
@@ -1,13 +1,5 @@
 package org.team9140.frc2025.subsystems;
 
-import static edu.wpi.first.units.Units.Inches;
-import static edu.wpi.first.units.Units.Kilograms;
-import static edu.wpi.first.units.Units.Meters;
-import static edu.wpi.first.units.Units.MetersPerSecond;
-import static edu.wpi.first.units.Units.Rotations;
-import static edu.wpi.first.units.Units.RotationsPerSecond;
-import static edu.wpi.first.units.Units.Seconds;
-
 import org.team9140.frc2025.Constants;
 import org.team9140.frc2025.Robot;
 import org.team9140.frc2025.Constants.Ports;
@@ -34,9 +26,11 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
+import static edu.wpi.first.units.Units.*;
+
 public class Elevator extends SubsystemBase {
-    private TalonFX leftMotor;
-    private TalonFX rightMotor;
+    private final TalonFX rightMotor;
+    private final TalonFX leftMotor;
 
     private final MotionMagicVoltage motionMagic;
 
@@ -53,8 +47,8 @@ public class Elevator extends SubsystemBase {
             Constants.Elevator.STOW_height.in(Meters));
 
     private Elevator() {
-        leftMotor = new TalonFX(Ports.ELEVATOR_MOTOR_LEFT, "sigma");
-        rightMotor = new TalonFX(Ports.ELEVATOR_MOTOR_RIGHT, "sigma");
+        this.rightMotor = new TalonFX(Ports.ELEVATOR_MOTOR_RIGHT, "sigma");
+        this.leftMotor = new TalonFX(Ports.ELEVATOR_MOTOR_LEFT, "sigma");
 
         Slot0Configs elevatorGains = new Slot0Configs()
                 .withKP(20.0)
@@ -94,9 +88,9 @@ public class Elevator extends SubsystemBase {
                 .withMotorOutput(motorOutputConfigs)
                 .withSoftwareLimitSwitch(softLimits);
 
-        this.leftMotor.getConfigurator().apply(motorConfig);
-        this.leftMotor.getConfigurator().apply(feedbackConfigs);
-        this.leftMotor.setPosition(0.0);
+        this.rightMotor.getConfigurator().apply(motorConfig);
+        this.rightMotor.getConfigurator().apply(feedbackConfigs);
+        this.rightMotor.setPosition(0.0);
 
         this.motionMagic = new MotionMagicVoltage(0)
                 .withEnableFOC(true)
@@ -104,7 +98,7 @@ public class Elevator extends SubsystemBase {
 
         this.targetPosition = Constants.Elevator.MIN_HEIGHT;
 
-        this.rightMotor.setControl(new Follower(this.leftMotor.getDeviceID(), true));
+        this.leftMotor.setControl(new Follower(this.rightMotor.getDeviceID(), true));
     }
 
     private static Elevator instance;
@@ -117,9 +111,9 @@ public class Elevator extends SubsystemBase {
     public void periodic() {
         SmartDashboard.putNumber("Elevator Current Position Inch", getPosition().in(Inches));
         SmartDashboard.putNumber("Elevator Target Position Inch", targetPosition.in(Inches));
-        SmartDashboard.putNumber("Elevator Voltage", leftMotor.getMotorVoltage().getValueAsDouble());
-        SmartDashboard.putNumber("Elevator Current", leftMotor.getStatorCurrent().getValueAsDouble());
-        SmartDashboard.putNumber("Elevator raw position", leftMotor.getPosition().getValueAsDouble());
+        SmartDashboard.putNumber("Elevator Voltage", rightMotor.getMotorVoltage().getValueAsDouble());
+        SmartDashboard.putNumber("Elevator Current", rightMotor.getStatorCurrent().getValueAsDouble());
+        SmartDashboard.putNumber("Elevator raw position", rightMotor.getPosition().getValueAsDouble());
         SmartDashboard.putBoolean("Elevator at target", this.atPosition.getAsBoolean());
         // SmartDashboard.putNumber("error",
         // this.leftMotor.getClosedLoopError().getValueAsDouble());
@@ -127,18 +121,18 @@ public class Elevator extends SubsystemBase {
 
     @Override
     public void simulationPeriodic() {
-        double simvVolts = this.leftMotor.getSimState().getMotorVoltage();
+        double simvVolts = this.rightMotor.getSimState().getMotorVoltage();
         this.elevatorSim.setInputVoltage(simvVolts);
         this.elevatorSim.update(Constants.LOOP_PERIOD.in(Seconds));
 
         Distance simPosition = Meters.of(this.elevatorSim.getPositionMeters());
         LinearVelocity simVelocity = MetersPerSecond.of(this.elevatorSim.getVelocityMetersPerSecond());
 
-        this.leftMotor.getSimState().setRawRotorPosition(
+        this.rightMotor.getSimState().setRawRotorPosition(
                 Rotations.of(simPosition.div(Constants.Elevator.SPOOL_CIRCUMFERENCE).magnitude()
                         * Constants.Elevator.GEAR_RATIO));
 
-        this.leftMotor.getSimState().setRotorVelocity(
+        this.rightMotor.getSimState().setRotorVelocity(
                 RotationsPerSecond
                         .of(simVelocity.div(Constants.Elevator.SPOOL_CIRCUMFERENCE).magnitude()
                                 * Constants.Elevator.GEAR_RATIO));
@@ -146,13 +140,13 @@ public class Elevator extends SubsystemBase {
 
     public Distance getPosition() {
         return Constants.Elevator.SPOOL_CIRCUMFERENCE
-                .times(this.leftMotor.getPosition().getValue().in(Rotations));
+                .times(this.rightMotor.getPosition().getValue().in(Rotations));
     }
 
     public Command moveToPosition(Distance goalPosition) {
         return this.runOnce(() -> {
             this.targetPosition = goalPosition;
-            this.leftMotor.setControl(this.motionMagic.withPosition(
+            this.rightMotor.setControl(this.motionMagic.withPosition(
                     this.targetPosition.div(Constants.Elevator.SPOOL_CIRCUMFERENCE).magnitude()));
         });
     }
