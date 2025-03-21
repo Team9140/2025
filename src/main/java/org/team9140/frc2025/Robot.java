33fd3e64f33d60c064b9311d64c7072d07d75bf8
Gijs Landwehr <gijslandwehr@gmail.com>
2025-03-21 12:35:30 -0500

    Stop SignalLogger
    Signed-off-by: Gijs Landwehr <gijslandwehr@gmail.com>

--- src/main/java/org/team9140/frc2025/Robot.java
+++ src/main/java/org/team9140/frc2025/Robot.java
@@ -36,7 +36,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void disabledInit() {
-        
+        SignalLogger.stop();
     }
 
     @Override
@@ -84,7 +84,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopExit() {
-        SignalLogger.stop();
+        
     }
 
     @Override
